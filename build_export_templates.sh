3874e43e59de55b18463b1a6166b29e13436afcf
The Benedani <18270989+Benedani@users.noreply.github.com>
2023-11-18 16:59:38 +0100

    Web: HiDPI blur fix

---
+++ build_export_templates.sh
@@ -0,0 +1,11 @@
+#scons p=x11 target=release tools=no
+
+scons p=android target=release tools=no android_arch=armv7
+scons p=android target=release tools=no android_arch=arm64v8
+scons p=android target=release tools=no android_arch=x86
+scons p=android target=release tools=no android_arch=x86_64
+cd platform/android/java/
+./gradlew generateGodotTemplates
+cd ../../../
+
+#scons p=javascript target=release tools=no
